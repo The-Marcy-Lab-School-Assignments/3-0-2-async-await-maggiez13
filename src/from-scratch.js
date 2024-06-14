f2b76f1486d2313ef358176443ffda26df50b25f
Mei Zhu <maggiez0613@gmail.com>
2024-06-14 18:11:29 -0400

    coding done

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,2 +1,25 @@
-export const fetchHandler = () => {
+export const fetchHandler = async (url, options ={}) => {
+  try {
+    const response = await fetch(url, options);
+    if (!response.ok) {
+      throw new Error(`Fetch failed with status - ${response.status}, ${response.status.text}`);
+    }
+    // checking if response is json 
+    const contentType = response.headers.get('content-type');
+    if (contentType !== null && contentType.includes('application/json')) {
+      const jsonData = await response.json();
+      return [jsonData, null]
+    }
+    // Get text response if not JSON
+    const textData = await response.text(); 
+    return [textData, null]; 
+    
+  } 
+  catch (error) {
+    console.warn(error);
+    return [null, error];
+  }
 };
+
+
+
